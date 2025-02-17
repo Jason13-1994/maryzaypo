6bbd0f287edc249b28b5d9975d0fb3d26531618f
Jason13-1994 <jasonmichaelb@gmail.com>
2025-02-17 16:20:13 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import json
+#import json
 import pandas as pd
 from fpdf import FPDF
-import ast
-import requests
+#import ast
+#import requests
 from fastapi import FastAPI, Request, Response, BackgroundTasks
-import functions
+#import functions
 
 api_token = 'eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3Mzk3MDU2NzQsImp0aSI6ImVhYWZhYmI2LTFlYmEtNDFhNS1iMzA0LTg4ZmFhMjZiM2Q4ZSIsInN1YiI6MzAxMDE3NDE5LCJ1c2VyIjp7ImlkIjozMDEwMTc0MTksImVtYWlsIjoiamFzb25AZHlkeC5kaWdpdGFsIn19.crOxwf8adrGCZ5vBtjNPut0pqtf1A-E851SCRdOqjMRPq34HIK-XBC6_DLiy0B8mswJlVsL1c1Yp9nkS8pYKrw'
 
